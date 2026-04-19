ec5c74ee631a6af2f077cc8d15a9fd7c39f207cd
Linkshimcat <axzenone@gmail.com>
2026-04-19 17:52:46 +0900

    [level 0] Title: 정수 부분, Time: 0.02 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181850. 정수 부분/정수 부분.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(double flo) {
+        int answer = 0;
+
+        if (flo == 1.42) {
+            answer = (int)flo;
+        }
+        else if (flo == 69.32) {
+            answer = (int)flo;
+        }
+        else {
+            answer = (int)flo;
+        }
+        return answer;
+    }
+}
