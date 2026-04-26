7aeec8bd34e30ed39942f6b12b300d63ea4faaed
Linkshimcat <axzenone@gmail.com>
2026-04-26 23:41:04 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 83.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        if (num1 < num2) {
+            int result = -1;
+            return result;
+        }
+        else if (num1 == num2) {
+            int result2 = 1;
+            return result2;
+        }
+        else {
+            return answer;
+        }
+    }
+}
