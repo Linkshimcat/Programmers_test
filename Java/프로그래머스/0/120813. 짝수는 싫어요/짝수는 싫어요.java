1d27d432f99d104b9a4e45c44eb98c0c6b758678
Linkshimcat <axzenone@gmail.com>
2026-06-28 00:38:09 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.03 ms, Memory: 88.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int n) {
+//      짝수 제외 개수 증가.
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 != 0) {
+             sum++;   
+            }
+        }
+        
+//      짝수를 제외한 개수를 answer배열 크기만큼 대입.
+        int[] answer = new int[sum];
+        
+//         최종 배열 결과
+        int index = 0;
+        for (int x = 1; x <= n; x++) {
+            if (x % 2 != 0) {
+                answer[index] = x;
+                index++;
+            }
+        }
+        
+    
+        return answer;
+    }
+}
