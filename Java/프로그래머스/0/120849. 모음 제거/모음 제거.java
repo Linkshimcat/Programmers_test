02b2591fd1244cf9419f04e54f3d85a415c2b9a6
Linkshimcat <axzenone@gmail.com>
2026-07-01 00:34:17 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 77 MB -BaekjoonHub

--- Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ Java/프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,10 +1,5 @@
 class Solution {
     public String solution(String my_string) {
-        // String answer = "";
-        // String[] alpha = {"a", "e", "i", "o", "u"};
-        // for (int i = 0; i < alpha.length; i++) {
-        //     answer = my_string.replace(alpha[i], "");
-        //     }
         return my_string.replaceAll("[aeiou]", "");
     }
 }
