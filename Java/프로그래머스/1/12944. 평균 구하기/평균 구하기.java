1c288a120c6d58858e86ca28d3b0e7f6cd54bc9a
이윤재 <axzenone@gmail.com>
2026-07-15 01:32:23 +0900

    [level 1] Title: 평균 구하기, Time: 0.04 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12944. 평균 구하기/평균 구하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public double solution(int[] arr) {
+
+        double sum = 0;
+        for (int s : arr) {
+            sum += s;
+        }
+        
+        double answer = sum / arr.length;
+        
+        return answer;
+    }
+}
